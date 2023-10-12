ed1e3b87e41c50844079a67212092d8575d32bb8
PeterSaan <saanpeter@gmail.com>
2023-10-12 11:24:49 +0300

    Fooling around

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { ref } from 'vue';
 
+const message = ref('Hello vue!') 
 </script>
 
 <template>
-  
+  <button @click="message='VueVue'">(C)Lick me</button>
+  <input type="text" v-model="message">
+  <h1>{{ message.split("").reverse().join("") }}</h1>
 </template>
