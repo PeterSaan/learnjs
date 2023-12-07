4f75b39bfe9be25428aa87ba6dc15cf69ebe6d1d
PeterSaan <saanpeter@gmail.com>
2023-12-07 10:16:24 +0200

    Mitme nädala stuff (23.11 järgi teha)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref, computed } from 'vue';
+import ItemList from './components/ItemList.vue';
 
 const message = ref('Hello vue!');
-const items = ref(['leib', 'sai', 'piim', 'viin']);
+let i = 1;
+const items = reactive([
+  { id: i++, name: 'leib', isDone: true },
+  { id: i++, name: 'sai', isDone: true },
+  { id: i++, name: 'piim', isDone: true },
+  { id: i++, name: 'kommid', isDone: true },
+]);
+
+const toDoItems = computed(() => {
+  return items.filter(item => !item.isDone);
+});
+const doneItems = computed(() => {
+  return items.filter(item => item.isDone);
+});
+
 function add() {
-  this.items.push(message)
+  items.push({id: i++, name: message.value, isDone: false});
 }
 </script>
 
 <template>
-  <button @click="add">(C)Lick me</button>
+  <button @click="add" class="button is-primary">(C)Lick me</button>
   <input type="text" v-model="message">
-  <h1>{{ message.split("").reverse().join("") }}</h1>
-  <ul>
-    <li v-for="item in items">{{ item }}</li>
-  </ul>
+  <ItemList :items="items" title="All items"></ItemList>
+  <ItemList :items="doneItems" title="Done items"></ItemList>
+  <ItemList :items="toDoItems" title="To do items"></ItemList>
 </template>
