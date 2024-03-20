42e5550238453861e35afac3a06f1cbb7544879c
PeterSaan <saanpeter@gmail.com>
2024-03-20 19:31:01 +0200

    Need to find a useful website to scrape :(

--- bin/homework/scraper.js
+++ bin/homework/scraper.js
@@ -2,3 +2,6 @@ import axios from "axios";
 import fs from "node:fs";
 
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+let res = await axios.post('https://www.neti.ee');
+console.log(res.data);
